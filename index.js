5336805ee18b371b0982bc773c820d2ce4cc4b3c
kazi430 <117241007+kazi430@users.noreply.github.com>
2022-12-20 16:37:57 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+document.querySelector("button").addEventListener("mouseover",myDemo);
+function myDemo (){
+
+    alert("Hello I am rady");
+
+}
+
+window.addEventListener("scroll", function(){
+
+console.log("Scroll");
+
+});
+
+
+window.addEventListener("resize", function(){
+
+console.log("resize");
+
+});
